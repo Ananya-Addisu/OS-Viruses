95538581d2c126cccb430f3c9b413d772a383993
Ananya Addisu <112194160+Ananya-Addisu@users.noreply.github.com>
2025-05-12 05:51:31 +0300

    Update ugly.bat

--- ugly.bat
+++ ugly.bat
@@ -1,5 +1,5 @@
 Set wshShell = wscript.CreateObject("WScript.Shell")
 do
 wscript.sleep 100
-wshshell.sendkeys "Muka Lu Jelek kayak Kontol Gue Waktu Ngatceng. Ikkeh... ikkeh... kimochi... crotz..."
+wshshell.sendkeys "This is a harmless virus"
 loop
